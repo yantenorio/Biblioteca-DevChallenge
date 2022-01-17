25a1699be0331805bd5ecc7e13e5d89fd424bdec
Yan Tenório da Silva <yan.silva@apece.company>
2022-01-17 15:28:33 -0300

    fixing errors

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-router.post('/obras')
+const bookController = require('../controllers/booksController');
+
+router.post('/obras', bookController.postBook)
 router.get('/obras')
 router.put('/obras/:id')
 router.put('/obras/:id')
--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
-const express = require('express')
+const express = require('express');
+const router = require('./routes/router');
 const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(router)
 
 app.listen(3000, () => {
     console.log('server running')
